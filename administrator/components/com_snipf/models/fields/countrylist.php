9d97fe5a6acb9a4445ed279bf0d02095bbc0f619
Lucas <lucas.sanner@gmail.com>
2018-09-14 21:38:14 +0200

    Adds citizenship language list.

--- administrator/components/com_snipf/models/fields/countrylist.php
+++ administrator/components/com_snipf/models/fields/countrylist.php
@@ -41,7 +41,7 @@ class JFormFieldCountryList extends JFormFieldList
     //Build the select options.
     foreach($countries as $country) {
       if($this->id == 'jform_citizenship') {
-	//$country->lang_var = $country->lang_var.'_CTZ';
+	$country->lang_var = $country->lang_var.'_CTZ';
       }
 
       $options[] = JHtml::_('select.option', $country->alpha_2, (empty($country->lang_var)) ? JText::_($country->name) : JText::_($country->lang_var));
