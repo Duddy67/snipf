e037429f90af5d596aca0734e7f290571a3e6db7
Lucas <lucas.sanner@codalia.net>
2019-09-02 12:24:13 +0000

    Fixes small bug in subscription list.

--- administrator/components/com_snipf/models/subscriptions.php
+++ administrator/components/com_snipf/models/subscriptions.php
@@ -133,7 +133,7 @@ class SnipfModelSubscriptions extends JModelList
     $search = $this->getState('filter.search');
     if(!empty($search)) {
       if(stripos($search, 'id:') === 0) {
-	$query->where('p.person_id = '.(int) substr($search, 3));
+	$query->where('s.person_id = '.(int) substr($search, 3));
       }
       else {
 	$search = $db->Quote('%'.$db->escape($search, true).'%');
